b0acb7585c9f596cb2ef0b18f31d272f4ee91b7c
topleft <pete.james.jeffryes@gmail.com>
2015-08-14 15:19:58 -0600

    admin map (formerly scraper) now inside app.js, both maps functioning

--- js/app.js
+++ js/app.js
@@ -1,105 +1,161 @@
 $(document).on("ready", function(){
-  console.log("app.js sanity");
 
-  var map;
-  var initialCenter = new google.maps.LatLng(39.393981, -106.016311);
-  var myOptions =
-    {
-    zoom: 6,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: initialCenter
-    }
-
-  map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-  // ----- Locations -------- //
+  var hasKannah = []
+  var adminMap;
 
 
-// Kannah Creek Brewing Company, Place ID: ChIJ81qEAEwcR4cRe2NQBSo1Vww
-// Edgewater Brewery, Place ID: ChIJI4bjvHAcR4cRNOwP0kGsGeU
-  // var hasKannahArray = ["ChIJI4bjvHAcR4cRNOwP0kGsGeU","ChIJ81qEAEwcR4cRe2NQBSo1Vww"]
-  var hasKannah = {
-      "Kannah Creek Brewing Company": {
-        name: "Kannah Creek",
-        placeId: "ChIJ81qEAEwcR4cRe2NQBSo1Vww"
-      },
-      "Lala's Wine Bar": {
-        name: "Lala's Wine Bar",
-        placeId: "ChIJjf6Sudl-bIcR3Uj_41fgHiY"
-      },
-      "Capitol Heights Pharmacy & Liquor": {
-        name: "Capitol Heights Pharmacy & Liquor",
-        placeId: "ChIJ6cmKPa9-bIcRmW5U1Hr0fOc"
+  // ----- Admin Locations Map -------- //
+
+  $("#admin").on("click", function(e){
+    e.preventDefault();
+    $(".admin-toggle").show();
+
+    var adminMap = new google.maps.Map(document.getElementById('admin-map'), {
+      center: {lat:39.393981, lng:-106.016311},
+      zoom: 7
+    });
+
+    var input = document.getElementById('admin-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', adminMap);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: adminMap
+    });
+
+    autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        return;
       }
-  }
+      if (place.geometry.viewport) {
+        adminMap.fitBounds(place.geometry.viewport);
+      } else {
+        adminMap.setCenter(place.geometry.location);
+        adminMap.setZoom(17);
+      }
+
+      // Set the position of the marker using the place ID and location.
+      marker.setPlace({
+        placeId: place.place_id,
+        location: place.geometry.location
+      });
+      marker.setVisible(true);
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+          'Place ID: ' + place.place_id + '<br>' +
+          place.formatted_address);
+      infowindow.open(adminMap, marker);
+
+
+      // add places to object for use in beer maps
+
+
+      hasKannah.push({"name":place.name, "placeId": place.place_id})
+
+
+    });
+  });
+
+
+  // *** locations object for testing  *** //
+  // var hasKannah = {
+  //     "Kannah Creek Brewing Company": {
+  //       name: "Kannah Creek",
+  //       placeId: "ChIJ81qEAEwcR4cRe2NQBSo1Vww"
+  //     },
+  //     "Lala's Wine Bar": {
+  //       name: "Lala's Wine Bar",
+  //       placeId: "ChIJjf6Sudl-bIcR3Uj_41fgHiY"
+  //     },
+  //     "Capitol Heights Pharmacy & Liquor": {
+  //       name: "Capitol Heights Pharmacy & Liquor",
+  //       placeId: "ChIJ6cmKPa9-bIcRmW5U1Hr0fOc"
+  //     }
+  // }
 
 
 
   // -----apply placesID's to map -------- //
-  var infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
 
 
-  function createMarker(place, map){
-    var marker = new google.maps.Marker({
-      "map": map,
-      position: place.geometry.location
-    });
-    return marker;
 
-  }
+  $("#find-beer").on("click", function(){
+    $(".find-beer-toggle").show();
+    console.log(hasKannah);
 
-  // get object from scraper
+    var infowindow = new google.maps.InfoWindow();
+    var initialCenter = new google.maps.LatLng(39.393981, -106.016311);
+    var map;
+    var myOptions =
+      {
+      zoom: 6,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: initialCenter
+      }
 
-  function populateMap(obj) {
-    var arr = Object.keys(obj);
-    for (var i = 0; i < arr.length; i++) {
-      //need to create an obj {placeId: id}
-      //this is bull shit...idObj[placeId] = obj[keys[i]][placeId];
-      var placeIdObject = {"placeId": obj[arr[i]].placeId}
-      service.getDetails(placeIdObject, function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          google.maps.event.addListener(
-              createMarker(place, map),
-              'click',
-                function() {
-                  infowindow.setContent(place.name, place.postal_code);
-                  infowindow.open(map, this);
-                }
-          );
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+    var service = new google.maps.places.PlacesService(map);
+
+    function createMarker(place, map){
+      var marker = new google.maps.Marker({
+        "map": map,
+        position: place.geometry.location
+      });
+      return marker;
+    }
+
+
+    function populateMap(arr) {
+      for (var i = 0; i < arr.length; i++) {
+        var currentId = arr[i].placeId;
+        service.getDetails({placeId: currentId}, function(place, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            google.maps.event.addListener(
+                createMarker(place, map),
+                'click',
+                  function() {
+                    infowindow.setContent(place.name, place.postal_code);
+                    infowindow.open(map, this);
+                  }
+            );
+          }
+        });
+      };
+    }
+
+
+    populateMap(hasKannah)
+
+  // -------- move map and zoom with zip code input -------- //
+    var geocoder;
+
+    function codeAddress() {
+      geocoder = new google.maps.Geocoder();
+      var address = document.getElementById("pac-input").value;
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          map.setZoom(14);
+          }
+        else {
+          alert("Geocode was not successful for the following reason: " + status);
         }
       });
     };
-  }
 
-
-  populateMap(hasKannah)
-
-  // -------- move map and zoom with zip code input -------- //
-  var geocoder;
-
-  function codeAddress() {
-    geocoder = new google.maps.Geocoder();
-    var address = document.getElementById("pac-input").value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        map.setZoom(14);
-        }
-      else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
+    $("form").on("submit", function(e){
+      e.preventDefault();
+      codeAddress();
     });
-  };
 
-  $("form").on("submit", function(e){
-    e.preventDefault();
-    codeAddress();
+
+
+  // closes event hanlder
   });
-
-
-
-
-
-
+// closes document on ready
 });
+
--- js/scraper.js
+++ js/scraper.js
@@ -2,20 +2,21 @@
 // store google place objects
 var places = {}
 
-$(document).ready(function(){
-    var map = new google.maps.Map(document.getElementById('map'), {
+$(document).on("ready", function(){
+  console.log("click sanity")
+    var scrapeMap = new google.maps.Map(document.getElementById('scraper-map'), {
       center: {lat:39.393981, lng:-106.016311},
       zoom: 7
     });
 
-    var input = document.getElementById('pac-input');
+    var input = document.getElementById('scraper-input');
 
     var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo('bounds', map);
+    autocomplete.bindTo('bounds', scrapeMap);
 
     var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker({
-      map: map
+      map: scrapeMap
     });
 
     autocomplete.addListener('place_changed', function() {
@@ -24,12 +25,11 @@ $(document).ready(function(){
       if (!place.geometry) {
         return;
       }
-
       if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
+        scrapeMap.fitBounds(place.geometry.viewport);
       } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);
+        scrapeMap.setCenter(place.geometry.location);
+        scrapeMap.setZoom(17);
       }
 
       // Set the position of the marker using the place ID and location.
@@ -42,10 +42,10 @@ $(document).ready(function(){
       infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
           'Place ID: ' + place.place_id + '<br>' +
           place.formatted_address);
-      infowindow.open(map, marker);
+      infowindow.open(scrapeMap, marker);
 
 
-      // add places to object for use in beer map
+      // add places to object for use in beer maps
       places[place.name] = {"name":place.name, "placeId": place.place_id}
       console.log(places);
 
