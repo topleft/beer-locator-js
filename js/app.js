f9b0c363b9b1fca3efa2659d1f9df31da34d83a5
topleft <pete.james.jeffryes@gmail.com>
2015-08-15 15:28:41 -0600

    set up more css, got glyphicons for navigation, and commented admin map

--- js/app.js
+++ js/app.js
@@ -10,39 +10,43 @@ $(document).on("ready", function(){
     e.preventDefault();
     $(".admin-toggle").show();
 
+    // create map
     var adminMap = new google.maps.Map(document.getElementById('admin-map'), {
       center: {lat:39.393981, lng:-106.016311},
       zoom: 7
     });
-    var place;
+    // grap form input
     var input = document.getElementById('admin-input');
-
+    // intitalize autocomplete on form intput
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', adminMap);
-
+    // instantiate info window
     var infowindow = new google.maps.InfoWindow();
+    // instantiate new marker specific to admin map
     var marker = new google.maps.Marker({
       map: adminMap
     });
+    // create scoped place variable
+    var place;
 
+    // listen for change in form input, assign value to place variable
     autocomplete.addListener('place_changed', function() {
       infowindow.close();
       place = autocomplete.getPlace();
     });
 
-    window.location.hash = "admin-input";
-    // window.location.hash = "";
-
-
-
+    // listen for user clicking button or hitting enter on keyboard
     $(".admin-toggle form").on("submit", function(e){
       e.preventDefault();
 
+      // check to see if place object has a location
       if (!place.geometry) {
         return;
       }
+      // check to see is that location is inside the current map view
       if (place.geometry.viewport) {
         adminMap.fitBounds(place.geometry.viewport);
+      // if not move the map view and make the place the center
       } else {
         adminMap.setCenter(place.geometry.location);
         adminMap.setZoom(17);
@@ -53,21 +57,27 @@ $(document).on("ready", function(){
         placeId: place.place_id,
         location: place.geometry.location
       });
+
+      //make marker visible on map
       marker.setVisible(true);
 
+      // fill in info window
       infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
           'Place ID: ' + place.place_id + '<br>' +
           place.formatted_address);
+
+      // display info window
       infowindow.open(adminMap, marker);
 
 
       // add places to object for use in beer maps
-
-
       hasKannah.push({"name":place.name, "placeId": place.place_id})
 
 
     });
+
+    // set view poert to admin map
+    window.location.hash = "admin-input";
   });
 
 
